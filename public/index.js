10d29122d2464223a43599677661286a874bfccf
juankboards <juankboards@gmail.com>
2019-04-15 23:04:27 -0400

    upload photo by photo

--- public/index.js
+++ public/index.js
@@ -125,7 +125,7 @@
 		}
 
 		function uploadImgBatch(imgs) {
-			let imgsBatch = imgs.splice(0, 5)
+			let imgsBatch = imgs.splice(0, 1)
 			let container = document.getElementById("album")
 			if(!imgsBatch.length) return
 			request("POST", `${window.location.pathname}/add`, { imgsBatch })
